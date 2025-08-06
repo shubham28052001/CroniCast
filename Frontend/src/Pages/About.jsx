90a0d99a5f9ecc9d8240b66145642a6a8bc37b92
shubham <shubhamsanodiya2805@gmail.com>
2025-08-06 14:49:19 +0530

    Fix: NavBar path issue

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../Components/NAvbar"
+import Navbar from "../Components/NavBar"
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Helmet } from "react-helmet";
